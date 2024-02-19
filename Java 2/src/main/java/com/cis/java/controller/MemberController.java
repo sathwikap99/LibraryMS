26eb26e2c9d1ec706969c4cffa28a3b767472cc3
sathwikap99 <sathwikaammu123@gmail.com>
2024-02-19 17:36:53 -0600

    library

---
+++ Java 2/src/main/java/com/cis/java/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.cis.java.controller;
+
+import com.cis.java.model.Member;
+import com.cis.java.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+    @RestController
+    @RequestMapping("/member")
+    public class MemberController {
+
+        @Autowired
+        private MemberService memberService;
+
+        // GET a member by ID
+        @GetMapping("/{memberId}")
+        public Member getMember(@PathVariable Long memberId) {
+            return memberService.getMember(memberId);
+        }
+
+        // Create a member
+        @PostMapping
+        public Member createMember(@RequestBody Member member) {
+            return memberService.createMember(member);
+        }
+
+        // Update a member
+        @PutMapping("/{memberId}")
+        public Member updateMember(@PathVariable Long memberId, @RequestBody Member member) {
+            return memberService.updateMember(memberId, member);
+        }
+
+        // Delete a member
+        @DeleteMapping("/{memberId}")
+        public void deleteMember(@PathVariable Long memberId) {
+            memberService.deleteMember(memberId);
+        }
+    }
+
